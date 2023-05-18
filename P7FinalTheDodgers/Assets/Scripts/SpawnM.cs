7552c34fcc444a49b22227241874b9cbaa413e3f
LaylaIJ <116124238+LaylaIJ@users.noreply.github.com>
2023-05-18 15:24:28 -0700

    fixed things up
    I fixed the traffic a little bit

--- P7FinalTheDodgers/Assets/Scripts/SpawnM.cs
+++ P7FinalTheDodgers/Assets/Scripts/SpawnM.cs
@@ -5,22 +5,30 @@ using UnityEngine;
 public class SpawnM : MonoBehaviour
 {
     public GameObject[] carPrefabs;
-    public int carIndex;
+    private float spawnRangeX = 20;
+    private float spawnPosZ = 20;
+    private float startDelay = 1;
+    private float spawnInterval = 1.5f;
+    private float spawnPosY = 20;
     
     // Start is called before the first frame update
     void Start()
     {
-        
+        InvokeRepeating("SpawnRandomCar", startDelay, spawnInterval);
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.E))
-        {
-            int carIndex = Random.Range(0, carPrefabs.Length);
+
+    }
+    
+    void SpawnRandomCar()
+    {
+        int carIndex = Random.Range(0, carPrefabs.Length);
+        Vector3 spawnPos = new Vector3(Random.Range(-spawnRangeX, spawnRangeX), 0, spawnPosZ);
             
-            Instantiate(carPrefabs[carIndex], new Vector3(Random.Range(20, 1, 145),carPrefabs[carIndex].transform.rotation);
-        }
+        Instantiate(carPrefabs[carIndex], new Vector3(20, 1, 145), carPrefabs[carIndex].transform.rotation);
+        
     }
 }
